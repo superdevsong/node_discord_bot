67df7470a312cf4961384ebc325b535a73a0d3e5
superdevsong <superdevsong@gmail.com>
2022-02-10 20:04:27 +0900

    second commit

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const Discord = require("discord.js");//discord 모듈 import 
 const config = require("./config.json");//설정파일 파싱 
-const ytdl = require('ytdl-core');//유튜브 video데이터를 다운받는것을 도와주는 모듈 
-const yts = require( 'yt-search' );//유튜브 검색을 도와주는 api 
+const play_dl = require('play-dl');//유튜브나 spotify 검색과 스트리밍을 도와주느 api 
+
 const { joinVoiceChannel ,createAudioPlayer,createAudioResource,AudioPlayerStatus } = require('@discordjs/voice');
 //join
 const prefix = "!";
-const queue = new Map();
+const queue = new Map(); // 노래정보를 담을 컬렉션 생성 guild.id와 songinfo를 key 와 value로 넣을 것이다.
 
 const client = new Discord.Client({intents: ["GUILDS","GUILD_VOICE_STATES","GUILD_MESSAGES"]});//객체를 넘김 넘김으로써 해당 권한을 줄거임
 
 
 
 
-client.on('messageCreate', (message) => {//event 리스너 등록
+client.on('messageCreate', (message) => {//event 리스너 등록 messageCreate는 message가 생성되어있을때 발생하는 이벤트이다.
     // message 작성자가 봇이면 그냥 returnm
     if (message.author.bot) return;
     // message 시작이 prefix가 아니면 return
     if (!message.content.startsWith(prefix)) return;
-    const serverQueue = queue.get(message.guild.id);
+    const serverQueue = queue.get(message.guild.id);//queue에 message.guild.id가 키로 있다면 리턴 없으면 undefined 초기에는 없을거다.
+    //문서상 guild는 채널과 사용자의 독립된 모음이라고 한다. ui에서는 이것을 서버라고 한다고 한다. 디스코드 서버를 말하며 guild.id는 디스코드 서버의 id이다.
+    //서버에 초대됨가 동시에 서버의 guild.id를 알수있으며 이를 이용해 맵으로 넣어놨다. 해당함수
     const commandBody = message.content.slice(prefix.length);//preifx의 length만큼 자름 
     const args = commandBody.split(' ');//띄어쓰기 단위로 나눠서 배열로 반환
     const command = args.shift().toLowerCase();//shift 배열의 첫번째요소를 잘라서 return함 당연히 현 배열은 첫번째요소가 없음 toLowerCase로 인해 반환할때는 소문자로 반환
-    console.log(`commandBody:${commandBody}\nargs:${args}\ncommand:${command}`);
     
     if (command === "ping") {
         message.reply(`pong!`);
@@ -46,7 +47,7 @@ client.on('messageCreate', (message) => {//event 리스너 등록
 
 async function execute(message, serverQueue) {//음악실행 함수 
     let title;
-    const args = message.content.slice(5);
+    const args = message.content.slice(6);
 
     const voiceChannel = message.member.voice.channel;//voice channel context 
     if (!voiceChannel)
@@ -59,11 +60,21 @@ async function execute(message, serverQueue) {//음악실행 함수
         "I need the permissions to join and speak in your voice channel!"
       );
     }
-    const video = (await yts(args)).videos.slice(0,1);//유튜브 검색후에 video 정보 배열 넘김 
-    const songInfo = await ytdl.getInfo(video[0].url);//video url 검색 
+    const songURL = await play_dl.yt_validate(args);//video url이 맞는지 검증  
+    let video = undefined;
+    let songInfo = undefined;
+    if(songURL=="search"){
+      video = await play_dl.search(args, {
+        limit: 1
+    });//유튜브 검색후에 video 정보 배열 넘김 
+      songInfo = await play_dl.video_info(video[0].url);//video url 검색 
+    } else {
+      songInfo = await play_dl.video_info(args);
+    }
+    
     const song = {
-          title: songInfo.videoDetails.title,
-          url: songInfo.videoDetails.video_url,
+          title: songInfo.video_details.title,
+          url: songInfo.video_details.url,
      };
   
     if (!serverQueue) {//처음에는 정의 되어 있지않으니 
@@ -134,7 +145,7 @@ async function execute(message, serverQueue) {//음악실행 함수
     serverQueue.player.unpause();
   }
   
-  function play(guild, song) {
+  async function play(guild, song) {
     const player = createAudioPlayer();
     const serverQueue = queue.get(guild.id);
     serverQueue.player = player;
@@ -143,7 +154,10 @@ async function execute(message, serverQueue) {//음악실행 함수
       queue.delete(guild.id);
       return;
     }
-    const resource = createAudioResource(ytdl(song.url));
+    let stream = await play_dl.stream(song.url);
+    const resource = createAudioResource(stream.stream, {
+      inputType: stream.type
+  });
     
     player.play(resource);
     player.on(AudioPlayerStatus.Idle, () => {
@@ -152,8 +166,7 @@ async function execute(message, serverQueue) {//음악실행 함수
     });
     player.on('error', error => {
       console.error(`Error2: ${error.message} \nError3:${error} `);
-      if(subscription)
-      getNextResource(guild,serverQueue,player);
+      
     });
     
     const subscription = serverQueue.connection
@@ -166,7 +179,7 @@ async function execute(message, serverQueue) {//음악실행 함수
     serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 
   }
-  function getNextResource(guild,serverQueue,player){//다음곡을 틀어주는 함수
+  async function getNextResource(guild,serverQueue,player){//다음곡을 틀어주는 함수
     serverQueue.songs.shift();
     if(!serverQueue.songs[0]){
          serverQueue.connection.destroy();//voicechannel과의 연결을 끊음
@@ -174,7 +187,10 @@ async function execute(message, serverQueue) {//음악실행 함수
         
       return;
     }
-    const resource = createAudioResource(ytdl(serverQueue.songs[0].url));
+    let stream = await play_dl.stream(song.url);
+    const resource = createAudioResource(stream.stream, {
+      inputType: stream.type
+  });
     player.play(resource);
   }
 
